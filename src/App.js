a5cb3dab54ef01398f56f52ace54f1ea6b128510
Praveen RS <praveenrs0111@gmail.com>
2021-07-19 23:47:44 +0530

    commit on 'master'

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 import AddFavourites from "./components/AddFavourites";
+import RemoveFavourites from "./components/RemoveFavourites";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [favourites, setFavourites] = useState([]);
 
   const getMovieRequest = async (searchValue) => {
     const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=2bc39733`;
@@ -25,6 +27,34 @@ const App = () => {
     getMovieRequest(searchValue);
   }, [searchValue]);
 
+  useEffect(() => {
+    const movieFavourites = JSON.parse(
+      localStorage.getItem("react-movie-app-favourites")
+    );
+
+    setFavourites(movieFavourites);
+  }, []);
+
+  const saveToLocalStorage = (items) => {
+    localStorage.setItem("react-movie-app-favourites", JSON.stringify(items));
+  };
+
+  const addFavouriteMovie = (movie) => {
+    const newFavouritesList = [...favourites, movie];
+    const newFavouritesListUnique = [...new Set(newFavouritesList)];
+    setFavourites(newFavouritesListUnique);
+    saveToLocalStorage(newFavouritesListUnique);
+  };
+
+  const removeFavouriteMovie = (movie) => {
+    const newFavouritesList = favourites.filter(
+      (favourite) => favourite.imdbID !== movie.imdbID
+    );
+
+    setFavourites(newFavouritesList);
+    saveToLocalStorage(newFavouritesList);
+  };
+
   return (
     <div className="container-fluid movie-app">
       <div className="row d-flex align-items-center mt-4 mb-4">
@@ -32,7 +62,21 @@ const App = () => {
         <SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
       </div>
       <div className="row">
-        <MovieList movies={movies} favouritesComponent={AddFavourites} />
+        <MovieList
+          movies={movies}
+          handleFavouritesClick={addFavouriteMovie}
+          favouritesComponent={AddFavourites}
+        />
+      </div>
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Favourites" />
+      </div>
+      <div className="row">
+        <MovieList
+          movies={favourites}
+          handleFavouritesClick={removeFavouriteMovie}
+          favouritesComponent={RemoveFavourites}
+        />
       </div>
     </div>
   );
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -5,7 +5,7 @@ const SearchBox = (props) => {
     <div className="col col-sm-4">
       <input
         className="form-control"
-        value={props.SearchValue}
+        value={props.searchValue}
         onChange={(event) => props.setSearchValue(event.target.value)}
         placeholder="Type the name of a movie..."
       ></input>
